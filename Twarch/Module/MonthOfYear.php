b96e23151bbcc5fe73a5a86e4cc0eb18e368941e
Tom Hudson <mail@tomnomnom.com>
2013-02-24 02:22:50 +0000

    Fixed monthofyear output heading (Day -> Month)

--- Twarch/Module/MonthOfYear.php
+++ Twarch/Module/MonthOfYear.php
@@ -29,7 +29,7 @@ class MonthOfYear extends \Twarch\Module {
       $days[$day]++;
     }
 
-    $fields = array('Day', 'Count');
+    $fields = array('Month', 'Count');
 
     $rows = array();
     foreach ($days as $day => $count){
