7df28682470276ecc064130a80bc2f851b4a5b0d
Tom Hudson <mail@tomnomnom.com>
2013-02-25 21:07:19 +0000

    Added --exclude-mentions option to uniquewords

--- Twarch/Module/UniqueWords.php
+++ Twarch/Module/UniqueWords.php
@@ -5,6 +5,7 @@ class UniqueWords extends \Twarch\Module {
   public function exec($args){
     $args->expectParam('--min-count');
     $args->expectParam('--min-word-length');
+    $args->expectFlag('--exclude-mentions');
     
     $minCount = 1;
     if ($args->paramIsSet('--min-count')){
@@ -14,6 +15,11 @@ class UniqueWords extends \Twarch\Module {
     $minWordLength = null;
     if ($args->paramIsSet('--min-word-length')){
       $minWordLength = (int) $args->getParamValue('--min-word-length');
+    }
+
+    $excludeMentions = false;
+    if ($args->flagIsSet('--exclude-mentions')){
+      $excludeMentions = true; 
     }
   
     $findTweets = $this->db->prepare('
@@ -34,6 +40,11 @@ class UniqueWords extends \Twarch\Module {
         // Check for minimum word length
         if (!is_null($minWordLength)){
           if (strLen($word) < $minWordLength) continue;
+        }
+
+        // Optionally exclude mentions
+        if ($excludeMentions){
+          if ($word[0] == '@') continue;
         }
 
         if (!isset($wordCounts[$word])){
